9f6ea7e5eb101aabfe733eab345521821f2b55dd
HyungsooLim <hyungsoolim0@gmail.com>
2022-08-02 21:58:18 +0900

    #8-1. dynamic template

--- src/controllers/videosController.ts
+++ src/controllers/videosController.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
-export const home = (req: any, res: any) => res.render("home");
+export const home = (req: any, res: any) =>
+	res.render("home", { pageTitle: "Home" });
 export const selectVideo = (req: any, res: any) => res.render("selectVideo");
-export const editVideo = (req: any, res: any) => res.send("edit video");
+export const editVideo = (req: any, res: any) => res.render("edit");
 export const search = (req: any, res: any) => res.send("search video");
 export const deleteVideo = (req: any, res: any) => res.send("delete video");
 export const upload = (req: any, res: any) => res.send("upload video");
