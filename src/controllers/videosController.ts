b3158c7c0f23f84414bfbddc119273e2a8f10baf
HyungsooLim <hyungsoolim0@gmail.com>
2022-08-03 23:16:13 +0900

    #8-2. template finished

--- src/controllers/videosController.ts
+++ src/controllers/videosController.ts
@@ -1,7 +1,39 @@
 import express from "express";
 
-export const home = (req: any, res: any) =>
-	res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+	username: "HSLim",
+	loggedIn: true,
+};
+
+export const home = (req: any, res: any) => {
+	const videos = [
+		{
+			title: "video #1",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+		{
+			title: "video #2",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+		{
+			title: "video #3",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+	];
+	return res.render("home", { pageTitle: "Home", videos });
+};
 export const selectVideo = (req: any, res: any) => res.render("selectVideo");
 export const editVideo = (req: any, res: any) => res.render("edit");
 export const search = (req: any, res: any) => res.send("search video");
